1c64930ddcfcb156a6119408c5a317bddbe57920
Eren Sönmez <erencpt11@gmail.com>
2024-04-08 01:16:32 +0300

    Update FacebookPage.java

--- src/test/java/pages/FacebookPage.java
+++ src/test/java/pages/FacebookPage.java
@@ -7,7 +7,7 @@ import utilities.Driver;
 
 public class FacebookPage {
 
-    public FacebookPage(){
+    public FacebookPage(){ 
         PageFactory.initElements(Driver.getDriver(),this);
     }
 
